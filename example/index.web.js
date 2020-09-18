940f3f2af70443b5e3cfda6d24b92a92c93b4359
Drew Grobengieser <drew@flowercitygaming.com>
2020-09-18 01:29:18 -0400

    updates

--- example/index.web.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { App } from './src/App/App';
-
-AppRegistry.registerComponent('App', () => App);
-AppRegistry.runApplication('App', {
-  rootTag: document.getElementById('react-root')
-});
